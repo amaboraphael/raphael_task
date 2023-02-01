4131602ca3de8b0bf5059ebcb3db29e6a9723cec
amaboraphael <raphaelamabo@gmail.com>
2023-02-02 00:27:53 +0200

    added exercise 2 with the headers and footer

---
+++ exercise2/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bon A p p é t i t - Team-13</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="homestyle.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+</head>
+  <body>
+
+  <div class="wrapper">
+  <div class="container">
+
+      <div class="row">
+        <div class="col-md-2">
+          <img class="logo" src="images/logo.png" alt="logo">
+        </div>
+
+        <div class="col-md-6">
+          <h1><b>Bon Appétit</b></h1>
+        </div>
+
+        <div class="col-md-4" style="text-align: right;">
+          <a class="btn btn-danger" href="about.html" role="button" style="vertical-align: middle ;"> <b>About us</b></a>
+          <a class="btn btn-danger" href="exercise2.php" role="button">exercise 1</a>
+          <a class="btn btn-danger" href="https://www.hamk.fi/" role="button" style="vertical-align: middle ;"> <b>Make Reservation</b></a>
+          <a class="home" href="index.html"><img class="home" src="images/home.png" alt="home button"> </a>
+        </div>
---
+++ layout/exercise2/exercise1.php
@@ -0,0 +1,50 @@
+<?php include "header.php"?>
+    <h1>My simple php script</h1>
+    <h3>3.1 Write a simple PHP script to print your information (Name and your groupid)</h3>
+<?php
+
+echo "Amabo Raphael Chenwi";
+echo "<br>";
+echo "BBCAP22 "
+
+?>
+ <h3>3.2.1 Write PHP code to display the following message.</h3>
+<?php 
+echo "Hello world! My name is \"David\""
+
+?>
+<h3>3.3 Write the PHP code in to display the current date. </h3>
+<?php  echo date("d.m.Y") ?>
+<h3>3.4 Variable in php. 
+</h3>
+
+<?php
+ $title ="PHP is interesting";
+echo "<h1>".$title."<h1>" ?>
+
+<h3>Table and Variables</h3>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+echo "
+<table>
+<tr>
+<th> S.N </th><th> Name </th><th>Grade</th>
+</tr>
+<tr>
+<td> 1 </td> <td> Pekka </td><td> $g1 </td>
+</tr>
+<tr>
+<td> 2 </td><td> Johanna </td><td> $g2 </td>
+</tr>
+<tr>
+<td> 2 </td><td> John </td><td> $g3 </td>
+</tr>
+</table>
+";
+?>
+
+<h3>4.0 screenshot that verifies, you have set up the development environment</h3>
+<img src="workenv.png"alt="working environment">
+<?php include "footer.php"?>
