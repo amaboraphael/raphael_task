b3fb03ff3d3c85cac26b2bec5f082e7f713cb3f2
amaboraphael <raphaelamabo@gmail.com>
2023-02-01 10:53:49 +0200

    exercise1 added text

--- header/index.php
+++ header/index.php
@@ -7,6 +7,8 @@ include "exercise1.php"
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et veritatis sed aspernatur blanditiis rerum est? Dolorum explicabo atque in dolores nulla quisquam porro at. Obcaecati sequi eius tempore quia facere?</p>
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et veritatis sed aspernatur blanditiis rerum est? Dolorum explicabo atque in dolores nulla quisquam porro at. Obcaecati sequi eius tempore quia facere?</p>
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et veritatis sed aspernatur blanditiis rerum est? Dolorum explicabo atque in dolores nulla quisquam porro at. Obcaecati sequi eius tempore quia facere?</p>
+
+<p> this is some new text to check</p>
 <?php 
 include "footer.php"
 ?>
